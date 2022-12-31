039b8628f6fcf73eeda394f9a0c85813fa8909c4
Shlok Srivastava <shlok@192.168.0.101>
2022-12-31 15:26:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Well. We are here Now! Mac is here, finally! Go conquer the world now!
         </p>
         <a
           className="App-link"
